8f2883a8d5f2e59856c54f945cbbd2a470c0d7fb
Raul <raulfernan000@gmail.com>
2022-06-01 08:15:35 +0200

    exercise done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
 import { Component } from '@angular/core';
+import { ToDoItem } from './toDoItem';
+import { ToDoList } from './toDoList';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'todo';
+
+  private list = new ToDoList('AA', [
+    new ToDoItem('Going to the grocery store', true),
+    new ToDoItem('Jogging'),
+    new ToDoItem('Traveling', false),
+  ]);
+
+  showComplete = false;
+
+  // username is an attribute that doesn´t have to be declared explicitly
+  get username(): string {
+    return this.list.user;
+  }
+
+  get itemCount(): number {
+    return this.list.items.filter(item => item.complete).length;
+  }
+
+  get items(): readonly ToDoItem[]{
+    return this.list.items;
+  }
+
+  addItem(newItem: string){
+    if (newItem!="") {
+      this.list.addItem(newItem)
+    }
+  }
+
 }
